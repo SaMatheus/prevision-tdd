f03c7fe8186e5def26a229ba2aa1a37ecb256f8e
SaMatheus <matheus.sa.alves@outlook.com>
2024-03-03 22:22:26 -0300

    Modularizando validadores

--- fizzbees.js
+++ fizzbees.js
@@ -1,18 +1,32 @@
 
 const fizzBees = (() => {
+  const validators = (number) => {
+    const equals0 = number === 0
+    const divisibleBy3 = number % 3 === 0
+    const divisibleBy5 = number % 5 === 0
+    const divisibleBy3and5 = divisibleBy3 && divisibleBy5
+
+    return {
+      equals0,
+      divisibleBy3,
+      divisibleBy5,
+      divisibleBy3and5
+    }
+  };
+
   const right = (number) => {
-    if (number === 0) return 'Não da pra dividir por 0'
-    if (number % 3 === 0 && number % 5 === 0) return 'fizzbees'
-    if (number % 3 === 0) return 'fizz'
-    if (number % 5 === 0) return 'bees'
+    if (validators(number).equals0) return 'Não da pra dividir por 0'
+    if (validators(number).divisibleBy3and5) return 'fizzbees'
+    if (validators(number).divisibleBy3) return 'fizz'
+    if (validators(number).divisibleBy5) return 'bees'
     else return number
   };
   
   const wrong = (number) => {
-    if (number === 0) return number
-    if (number % 3 === 0 && number % 5 === 0) return number
-    if (number % 3 === 0) return number
-    if (number % 5 === 0) return number
+    if (validators(number).equals0) return number
+    if (validators(number).divisibleBy3and5) return number
+    if (validators(number).divisibleBy3) return number
+    if (validators(number).divisibleBy5) return number
     else return 'fezzbees'
   };
 
